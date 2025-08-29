025ce2d998bb15c2ee86d822c494bd063dc5a8a7
Hatsuga-HOST <rilhmkrn@gmail.com>
2025-08-29 22:15:27 +0700

    Update KeySystem.lua

--- KeySystem.lua
+++ KeySystem.lua
@@ -1,1 +1,16 @@
+-- KeySystem.lua
+local KeySystem = {}
 
+KeySystem.Key = "ZiaanHubKey123" -- ganti key sesuai keinginan
+KeySystem.Access = false
+
+function KeySystem:Check(input)
+    if input == self.Key then
+        self.Access = true
+        return true
+    else
+        return false
+    end
+end
+
+return KeySystem
