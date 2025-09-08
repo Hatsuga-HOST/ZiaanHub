873256808b2e99d726e37d54369a8e8f2b17620b
Hatsuga-HOST <rilhmkrn@gmail.com>
2025-09-08 14:50:31 +0700

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [102234703920418] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Gunung Daun Nih Brek
+  [102234703920418] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungDaun/utm.lua", -- Gunung Daun Nih Brek
 }
 
 return Games
