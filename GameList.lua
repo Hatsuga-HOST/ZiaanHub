d981f85baf42ee3387d4c021a27633a0d9d1c4bc
ZiaanFounder <rilhmkrn@gmail.com>
2025-09-08 16:33:07 +0700

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [102234703920418] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungDaun/utm.lua", -- Gunung Daun Nih Brek
+  [102234703920418] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungDaun/utm.lua", -- [[ Gunung Daun Nih Brek : www.roblox.com/id/games/102234703920418/EVENT-Mount-Daun ]] --
 }
 
 return Games
