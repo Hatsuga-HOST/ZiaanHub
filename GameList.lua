9f8c82a1fc836b77df36f7f3910ecab4d7d1b376
ZiaanFounder <rilhmkrn@gmail.com>
2025-09-11 12:19:19 +0700

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [86076978383613] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Hunty%20Zombie.lua", -- Mount Daun
+  [86076978383613] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungDaun/utm.lua", -- Mount Daun
   [121864768012064] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Fish%20It.lua", -- Mount Sakahayang
 }
 
