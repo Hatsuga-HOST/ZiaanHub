c71eaeebf359083befa399c1f8baeb81be4523ca
ZiaanFounder <rilhmkrn@gmail.com>
2025-09-11 11:44:49 +0700

    Update ZiaanHub.lua

--- ZiaanHub.lua
+++ ZiaanHub.lua
@@ -1,7 +1,13 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/GameList.lua"))()
+local games = {
+    [102234703920418] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungDaun/utm.lua",
+    [137123819476589] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungSakahayang/Csx.lua",
+}
 
-local URL = Games[game.PlaceId]
+local currentID = game.PlaceId
+local scriptURL = games[currentID]
 
-if URL then
-  loadstring(game:HttpGet(URL))()
+if scriptURL then
+    loadstring(game:HttpGet(scriptURL))()
+else
+    game.Players.LocalPlayer:Kick("Yo! This game ain't on the list.\nCheck the Discord for whitelisted games, homie.")
 end
