f774b6e513f7b8e8ffa68329eefd84206291e7ef
ZiaanFounder <rilhmkrn@gmail.com>
2025-09-11 12:21:59 +0700

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,1 +1,6 @@
-https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungSakahayang/Csx.lua
+local Games = {
+  [86076978383613] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Hunty%20Zombie.lua",
+  [121864768012064] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/refs/heads/main/Fish%20It.lua",
+}
+
+return Games
