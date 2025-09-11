28c8537f22540f4273c9379514f893dd35823295
ZiaanFounder <rilhmkrn@gmail.com>
2025-09-11 12:35:00 +0700

    Delete GameList.lua

--- GameList.lua
+++
@@ -1,6 +0,0 @@
-local Games = {
-  [102234703920418] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungDaun/utm.lua",
-  [137123819476589] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungSakahayang/Csx.lua",
-}
-
-return Games
