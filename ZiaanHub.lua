a3bfb183193109aa353824befec1c0706b6b5dca
ZiaanFounder <rilhmkrn@gmail.com>
2025-09-11 12:35:11 +0700

    Delete ZiaanHub.lua

--- ZiaanHub.lua
+++
@@ -1,7 +0,0 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/GameList.lua"))()
-
-local URL = Games[game.PlaceId]
-
-if URL then
-  loadstring(game:HttpGet(URL))()
-end
