709e5ab590331cf4fbf5367cab7e00e557a75b70
ZiaanFounder <rilhmkrn@gmail.com>
2025-09-11 11:20:59 +0700

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,6 +1,8 @@
 local Games = {
-  [102234703920418] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungDaun/utm.lua", -- [[ Gunung Daun / Mount Daun : www.roblox.com/id/games/102234703920418/EVENT-Mount-Daun ]] --
-  [137123819476589] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungSakahayang/Csx.lua", -- [[ Gunung Sakahayang / Mount Sakahayang : www.roblox.com/id/games/137123819476589/UPDATE-Gunung-Sakahayang ]] --
+  [102234703920418] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungDaun/utm.lua", 
+  -- [[ Gunung Daun / Mount Daun : www.roblox.com/id/games/102234703920418/EVENT-Mount-Daun ]]
+  [137123819476589] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungSakahayang/Csx.lua",
+  -- [[ Gunung Sakahayang / Mount Sakahayang : www.roblox.com/id/games/137123819476589/UPDATE-Gunung-Sakahayang ]]
 }
 
 return Games
