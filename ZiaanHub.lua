6433a62b9d5d20a9315829bbcfb159fd70f77aa9
ZiaanFounder <rilhmkrn@gmail.com>
2025-09-14 23:33:34 +0700

    Update ZiaanHub.lua

--- ZiaanHub.lua
+++ ZiaanHub.lua
@@ -1,4 +1,4 @@
-local Gunung = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"))()
+local Gunung = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Mount.lua"))()
 local Tower = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"))()
 
 local URL = Gunung[game.PlaceId]
