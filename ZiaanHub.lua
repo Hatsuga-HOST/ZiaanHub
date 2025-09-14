b2b1d551e3bfdd6d276d41828485664afef7b336
ZiaanFounder <rilhmkrn@gmail.com>
2025-09-14 23:31:15 +0700

    Create ZiaanHub.lua

---
+++ ZiaanHub.lua
@@ -0,0 +1,9 @@
+local Gunung = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"))()
+local Tower = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"))()
+
+local URL = Gunung[game.PlaceId]
+local URL = Tower[game.PlaceId]
+
+if URL then
+  loadstring(game:HttpGet(URL))()
+end
