3250ca8a6beb3519fe5a93f68309ac4ce5155d68
ZiaanFounder <rilhmkrn@gmail.com>
2025-09-14 23:48:46 +0700

    Update Mount.lua

--- Mount.lua
+++ Mount.lua
@@ -4,7 +4,7 @@ local Gunung = {
   [102234703920418] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungDaun/utm.lua", -- Mount Daun
   [137123819476589] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungSakahayang/Csx.lua", -- Mount Sakahayang 
   [130117824356035] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungNisara/Gwa.lua",  -- Mount Nisara
-  [16498369169] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/PS99.lua",  -- PS99 World 2
+  [75763972512400] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungRamah/Krptorngentd.lua",  -- "Mount Ramah"
 }
 
 return Gunung
