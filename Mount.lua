1306d44caa5c8cb021a640772043a5d4a728510e
ZiaanFounder <rilhmkrn@gmail.com>
2025-09-14 23:40:06 +0700

    Update Mount.lua

--- Mount.lua
+++ Mount.lua
@@ -2,7 +2,7 @@ local Gunung = {
   [123224294054165] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungAtin/O.lua", -- Mount Atin
   [91113397015653] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungAzurai/lol.lua", -- Mount Azuria
   [102234703920418] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungDaun/utm.lua", -- Mount Daun
-  [3623096087] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Muscle%20Legends.lua", -- Muscle Legends
+  [137123819476589] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungSakahayang/Csx.lua", -- Mount Sakahayang 
   [8737899170] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/PS99.lua",  -- PS99 World 1
   [16498369169] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/PS99.lua",  -- PS99 World 2
 }
