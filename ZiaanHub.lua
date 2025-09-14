93738c0b24498138903ac7a5c2dcaa3a0923214a
ZiaanFounder <rilhmkrn@gmail.com>
2025-09-14 23:54:34 +0700

    Update ZiaanHub.lua

--- ZiaanHub.lua
+++ ZiaanHub.lua
@@ -1,9 +1,10 @@
 local Gunung = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Mount.lua"))()
-local Tower = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"))()
+local Tower = loadstring(game:HttpGet("https://github"))()
 
-local URL = Gunung[game.PlaceId]
-local URL = Tower[game.PlaceId]
+local URL = Gunung[game.PlaceId] or Tower[game.PlaceId]
 
 if URL then
-  loadstring(game:HttpGet(URL))()
+    loadstring(game:HttpGet(URL))()
+else
+    warn("Error Bro")
 end
