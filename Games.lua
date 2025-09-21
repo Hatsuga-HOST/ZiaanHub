fd7ac729c002ebc377c9394d124fc3481c0d65a2
ZiaanFounder <rilhmkrn@gmail.com>
2025-09-21 13:34:38 +0700

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [123224294054165] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungAtin/O.lua", -- PoopABrainRot
+  [123224294054165] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Idk/Ziaan/PoopABrainRot/Lol.lua", -- PoopABrainRot
 }
 
 return Games
