fd59521a2067be1bb0fdfda812031f0fd4bdf8fb
ZiaanFounder <rilhmkrn@gmail.com>
2025-09-21 13:33:52 +0700

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,1 +1,5 @@
+local Games = {
+  [123224294054165] = "https://raw.githubusercontent.com/Hatsuga-HOST/ZiaanHub/refs/heads/main/Gunung/AutoSummit/GunungAtin/O.lua", -- PoopABrainRot
+}
 
+return Games
